05a5e4233b3bf3cca0496260c935fb8995447637
Daniel DeFreez <daniel@defreez.com>
2012-01-25 20:44:39 -0800

    added busybox - bad idea to put this here, but it is expedient until we include real busybox build

--- full_crespo.mk
+++ full_crespo.mk
@@ -22,6 +22,9 @@
 # Get the long list of APNs
 PRODUCT_COPY_FILES := device/sample/etc/apns-full-conf.xml:system/etc/apns-conf.xml
 
+# This should go somewhere else - just here for testing
+PRODUCT_COPY_FILES += device/samsung/crespo/busybox:system/bin/busybox
+
 # Camera
 PRODUCT_PACKAGES := \
     Camera
